239479d858d194896f53c32ac283f723a7f138bb
Vladislav <81467635+grGred@users.noreply.github.com>
2022-02-10 16:13:28 +0300

    Small solhint changes

--- Wallet.sol
+++ Wallet.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  >=0.7.0 <0.9.0;
+pragma solidity  ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/IERC20.sol";
 
 contract Wallet {
     
-    address payable owner;
+    address payable public owner;
     // Allowance for sending Eth
     mapping(address => mapping(address => uint256)) public allowanceEth;
     // Allowance for sending Tokens
@@ -13,11 +13,11 @@ contract Wallet {
     // Mapping of all balances
     mapping(address => uint) private balanceOf;
     // Start comission is 0.1 %
-    uint storedComission = 1;
+    uint public storedComission = 1;
     // Constant address that will recieve comission
-    address constant comissionReciverAddress = 0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC; 
-    uint comissionAmount;
-    uint recipientRecives;
+    address constant internal COMISSION_RECIVER_ADDRESS = 0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC; 
+    uint internal comissionAmount;
+    uint internal recipientRecives;
 
     // Block with events
     // Keep track of deposited funds
@@ -34,7 +34,7 @@ contract Wallet {
     event ApprovalEth(address owner, address spender, uint256 value);
     event ApprovalToken(address owner, address spender, IERC20 token, uint256 value);
     
-    constructor() public payable{
+    constructor() public payable {
         owner = payable(msg.sender);
     }
 
@@ -46,7 +46,7 @@ contract Wallet {
 
     /// @dev external function that accept all the values to a smartcontract
     //  event Deposit
-    function deposit() payable external {
+    function deposit() external payable {
         balanceOf[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value);
     }
@@ -82,17 +82,17 @@ contract Wallet {
     /// @dev withdraw Eth only by token holder
     /// @param _amount amount value of Eth to send
     //  recipientRecives amount of Eth recipient recieves except comission
-    //  comissionAmount amount of comission comissionReciverAddress recieves
-    //  Transfer Funds to recipient and comissionReciverAddress 
+    //  comissionAmount amount of comission COMISSION_RECIVER_ADDRESS recieves
+    //  Transfer Funds to recipient and COMISSION_RECIVER_ADDRESS 
     //  WithdrawFunds event
     //  transfer Eth back to wallet
-    function withdrawEthSupportingFee(uint _amount) payable external {
+    function withdrawEthSupportingFee(uint _amount) external payable {
         require(balanceOf[msg.sender] >= _amount, "Insufficient amount");
         (recipientRecives, comissionAmount) = getComission(_amount);
-        address payable _withdraw_address = payable(msg.sender);
-        _withdraw_address.transfer(recipientRecives);
-        _transfer(msg.sender, payable(comissionReciverAddress), comissionAmount);
-        emit WithdrawFunds(_withdraw_address, recipientRecives);
+        address payable _withdrawAddress = payable(msg.sender);
+        _withdrawAddress.transfer(recipientRecives);
+        _transfer(msg.sender, payable(COMISSION_RECIVER_ADDRESS), comissionAmount);
+        emit WithdrawFunds(_withdrawAddress, recipientRecives);
     }
     
     /// @dev transfer by approved person from original address of an amount within approved limit 
@@ -100,21 +100,21 @@ contract Wallet {
     /// @param _recipient receiver of Eth
     /// @param _amount amount value of Eth to send
     //  recipientRecives amount of Eth recipient recieves except comission
-    //  comissionAmount amount of comission comissionReciverAddress recieves
-    //  Transfer Funds to recipient and comissionReciverAddress 
+    //  comissionAmount amount of comission COMISSION_RECIVER_ADDRESS recieves
+    //  Transfer Funds to recipient and COMISSION_RECIVER_ADDRESS 
     //  TransferFundsWithAllowance event
     //  Allow _spender to spend up to _amount on your behalf
     function fromTransferEthSupportingFee(
         address _from, 
         address payable _recipient, 
         uint256 _amount
-        ) payable external {
+        ) external payable {
         require(balanceOf[_from] >= _amount, "Sender insufficient funds");
         require(allowanceEth[_from][msg.sender] >= _amount, "Not enough approved tokens");
         (recipientRecives, comissionAmount) = getComission(_amount);
         allowanceEth[_from][msg.sender] -= (_amount);
         _transfer(_from, _recipient, recipientRecives);
-        _transfer(_from, payable(comissionReciverAddress), comissionAmount);
+        _transfer(_from, payable(COMISSION_RECIVER_ADDRESS), comissionAmount);
         emit TransferFundsWithAllowance(_from, _recipient, recipientRecives);
     }
     
@@ -122,18 +122,18 @@ contract Wallet {
     /// @param _recipient receiver of Eth
     /// @param _amount amount value of Eth to send
     //  recipientRecives amount of Eth recipient recieves except comission
-    //  comissionAmount amount of comission comissionReciverAddress recieves
-    //  Transfer Funds to recipient and comissionReciverAddress
+    //  comissionAmount amount of comission COMISSION_RECIVER_ADDRESS recieves
+    //  Transfer Funds to recipient and COMISSION_RECIVER_ADDRESS
     //  TransferFunds event
     //  Allow funds owner spend up his Eth
     function transferEthSupportingFee(
         address payable _recipient, 
         uint _amount
-        ) payable external {
+        ) external payable {
         require(balanceOf[msg.sender] >= _amount, "Insufficient funds");
         (recipientRecives, comissionAmount) = getComission(_amount);
         _transfer(msg.sender, _recipient, recipientRecives);
-        _transfer(msg.sender, payable(comissionReciverAddress), comissionAmount);
+        _transfer(msg.sender, payable(COMISSION_RECIVER_ADDRESS), comissionAmount);
         emit TransferFunds(msg.sender, _recipient, recipientRecives);
     }
 
@@ -143,8 +143,8 @@ contract Wallet {
     }
 
     /// @notice Checks balance of Tokens in contract
-    function walletBalanceToken(IERC20 _token_addr) public view returns(uint) {
-      return (_token_addr.balanceOf(address(this)));
+    function walletBalanceToken(IERC20 _tokenAddr) public view returns(uint) {
+      return (_tokenAddr.balanceOf(address(this)));
     }
 
     /// @dev Approves amount of Eth that spender can use
@@ -160,51 +160,51 @@ contract Wallet {
 
     /// @dev Approves amount of Tokens that spender can use
     /// @param _spender address that can use our Tokens
-    /// @param _token_addr token address we are ready to share
+    /// @param _tokenAddr token address we are ready to share
     /// @param _value amount value of Eth to share with address
     //  event ApprovalToken
-    function approveToken(address _spender, IERC20 _token_addr, uint _value) external {
+    function approveToken(address _spender, IERC20 _tokenAddr, uint _value) external {
         require(_spender != msg.sender, "You are already allowed to move your Tokens");
-        require(_token_addr.balanceOf(address(this)) >= _value, "You cannot approve more tokens then you have");
-        allowanceToken[msg.sender][_spender][_token_addr] = _value;
-        emit ApprovalToken(msg.sender, _spender, _token_addr, _value);
+        require(_tokenAddr.balanceOf(address(this)) >= _value, "You cannot approve more tokens then you have");
+        allowanceToken[msg.sender][_spender][_tokenAddr] = _value;
+        emit ApprovalToken(msg.sender, _spender, _tokenAddr, _value);
     }
 
     /// @dev transfer ERC20 tokens to address
-    /// @param _token_addr token address we are sending
+    /// @param _tokenAddr token address we are sending
     /// @param _to token recipient 
     /// @param _amount amount of tokens we are sending
     //  event TransferERC20
     function transferERC20(
-        IERC20 _token_addr, 
+        IERC20 _tokenAddr, 
         address _to, 
         uint _amount
         ) external { 
-        require(_token_addr.balanceOf(address(this)) >= _amount, "Insufficient funds");
+        require(_tokenAddr.balanceOf(address(this)) >= _amount, "Insufficient funds");
         (recipientRecives, comissionAmount) = getComission(_amount);
-        _token_addr.transfer(_to, recipientRecives);
-        _token_addr.transfer(comissionReciverAddress, comissionAmount);
-        emit TransferERC20(_token_addr, msg.sender, _to, recipientRecives);
+        _tokenAddr.transfer(_to, recipientRecives);
+        _tokenAddr.transfer(COMISSION_RECIVER_ADDRESS, comissionAmount);
+        emit TransferERC20(_tokenAddr, msg.sender, _to, recipientRecives);
     }
 
     /// @dev transfer ERC20 tokens to address
-    /// @param _token_addr token address we are sending
+    /// @param _tokenAddr token address we are sending
     /// @param _from address sending Tokens
     /// @param _to token recipient 
     /// @param _amount amount of tokens we are sending
     //  event TransferERC20WithAllowance
     function fromTransferERC20(
-        IERC20 _token_addr, 
+        IERC20 _tokenAddr, 
         address _from, 
         address _to, 
         uint _amount
         ) external { 
-        require(_token_addr.balanceOf(address(this)) >= _amount, "Insufficient funds");
-        require(allowanceToken[_from][msg.sender][_token_addr] >= _amount, "Not enough approved tokens");
-        allowanceToken[_from][msg.sender][_token_addr] -= _amount;
+        require(_tokenAddr.balanceOf(address(this)) >= _amount, "Insufficient funds");
+        require(allowanceToken[_from][msg.sender][_tokenAddr] >= _amount, "Not enough approved tokens");
+        allowanceToken[_from][msg.sender][_tokenAddr] -= _amount;
         (recipientRecives, comissionAmount) = getComission(_amount);
-        _token_addr.transfer(_to, recipientRecives);
-        _token_addr.transfer(comissionReciverAddress, comissionAmount);
-        emit TransferERC20WithAllowance(_token_addr, msg.sender, _from, _to, recipientRecives);
+        _tokenAddr.transfer(_to, recipientRecives);
+        _tokenAddr.transfer(COMISSION_RECIVER_ADDRESS, comissionAmount);
+        emit TransferERC20WithAllowance(_tokenAddr, msg.sender, _from, _to, recipientRecives);
     }
 }
